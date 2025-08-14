580413ceb06eb83d659efb115b75e85e6b55e7aa
Freischutz <xlxixsxa4@gmail.com>
2025-08-15 02:09:21 +0700

    fdd

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,11 @@
 import { serve } from "inngest/next";
-import { createUserOder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { createUserOder, inngest, syncUserCreation, syncUserDeletion} from "@/config/inngest";
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
    syncUserCreation,
-   syncUserUpdation,
    syncUserDeletion,
    createUserOder
   ],
